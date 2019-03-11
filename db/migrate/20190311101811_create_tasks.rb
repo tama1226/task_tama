0aefbff4fdd95d13466efb943cfb15f82c9744c7
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-03-11 22:33:36 +0900

    Taskテーブル修正（カラム名）

--- db/migrate/20190311101811_create_tasks.rb
+++ db/migrate/20190311101811_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :task_name
+      t.string :name
       t.text :content
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2019_03_11_101811) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_name"
+    t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
