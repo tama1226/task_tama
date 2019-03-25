ab9c968a67c5deecc5363b3f1124b0b52923bd16
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-03-25 11:04:27 +0900

    タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module TaskTama
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: true,
