852f1ee78854491873c8937b1aba244ad75d3d1f
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-04-06 07:47:37 +0900

    詳細テスト・並び替えテスト部分の修正

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -4,14 +4,14 @@ FactoryBot.define do
     # 作成するテストデータの名前を「task」とします
     # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
     factory :task do
-      name { 'Factoryで作ったデフォルトのタイトル１' }
-      content { 'Factoryで作ったデフォルトのコンテント１' }
+      name { 'sampleタイトル1' }
+      content { 'sample詳細1' }
     end
   
     # 作成するテストデータの名前を「second_task」とします
     # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
     factory :second_task, class: Task do
-      name { 'Factoryで作ったデフォルトのタイトル２' }
-      content { 'Factoryで作ったデフォルトのコンテント２' }
+      name {'sampleタイトル2' }
+      content { 'sample詳細2' }
     end
   end
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -9,60 +9,44 @@ RSpec.feature "タスク管理機能", type: :feature do
   FactoryBot.create(:task)
   FactoryBot.create(:second_task)
   end
-end
 
   scenario "タスク一覧のテスト" do
   # tasks_pathにvisitする（タスク一覧ページに遷移する）
     visit tasks_path
-
-  # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
-  # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
-    expect(page).to have_content 'testtesttest'
-    expect(page).to have_content 'samplesample'
+  # タスク一覧ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+    expect(page).to have_content 'sampleタイトル1' 
+    expect(page).to have_content 'sample詳細1'
   end
 
   scenario "タスク作成のテスト" do
-    # new_task_pathにvisitする（タスク登録ページに遷移する）
-  # 1.ここにnew_task_pathにvisitする処理を書く
+  # new_task_pathにvisitする（タスク登録ページに遷移する）
     visit new_task_path
-
-  # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
   # タスクのタイトルと内容をそれぞれfill_in（入力）する
-  # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
     fill_in 'タスク名',with:'work'
-
-  # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
     fill_in '詳細',with:'practice'
 
-  # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
-  # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+  #「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
     click_on '登録する'
 
   # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-  # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-  # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+  # タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
     expect(page).to have_content 'work' 
     expect(page).to have_content 'practice'
   end
 
   scenario "タスク詳細のテスト" do
-  # あらかじめタスク詳細のテストで使用するためのタスクを仮作成
-    task = Task.create!(name: 'test_task_03', content: 'test3')
-
   # タスク詳細ページに遷移
     visit task_path(task[:id])
-
-  # visitした（到着した）expect(page)に（タスク詳細ページに）「test_task_03」「test3」という文字列が
-  # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テスト
-  # 詳細ページに遷移したことを確認したいので「各タスク詳細画面」という文字列の含みも確認
-    expect(page).to have_content '各タスク詳細画面'
-    expect(page).to have_content 'test_task_03'
-    expect(page).to have_content 'test3'
+  # タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+    expect(page).to have_content 'sampleタイトル1'
+    expect(page).to have_content 'sample詳細1'
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
   #タスクが作成日時の降順に並んでいる
-    Task.all.order(created_at: :desc)
-  
+  Task.create(id: 1, name: 'sampleタイトル1', updated_at: Time.current + 1.days)
+  Task.create(id: 2, name: 'sampleタイトル2', updated_at: Time.current + 2.days)
+
+  expect(Task.order("updated_at DESC").map(&:id)).to eq [2,1]
   end
 end
