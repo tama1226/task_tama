c03f4fc8e917416793aa0f3e506e7a28ffb3fd32
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-04-21 14:41:29 +0900

    バリデーションのテスト作成

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+
+  it "nameが空ならバリデーションが通らない" do
+    task = Task.new(name: '', content: '詳細')
+    expect(task).not_to be_valid
+  end
+
+  it "contentが空ならバリデーションが通らない" do
+    task = Task.new(name: '名前', content: '')
+    expect(task).not_to be_valid
+  end
+
+  it "titleとcontentに内容が記載されていればバリデーションが通る" do
+    task = Task.new(name: '名前', content: '詳細')
+    expect(task).to be_valid
+    
+  end
+end
