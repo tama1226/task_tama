b75ede7093574cc2fe955a5fd97c4fbe63522a7d
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-09 23:01:24 +0900

    検索インデックスを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_072931) do
+ActiveRecord::Schema.define(version: 2019_05_09_135726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_05_05_072931) do
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: -> { "CURRENT_TIMESTAMP" }
     t.string "status", default: "未着手"
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
