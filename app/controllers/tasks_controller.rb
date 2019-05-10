7d94924df8742a841ffbfad55348a146b4ad58f8
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-10 09:40:10 +0900

    view部分の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:name,:content,:deadline,:status)
+    params.require(:task).permit(:name,:content,:deadline,:status,:priority)
   end
 
   def set_task
