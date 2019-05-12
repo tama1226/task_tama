137ec6b03b6af4f40e83cc73b3ad78f8d0cfda66
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-13 08:47:10 +0900

    scopeを使用した場合の検索テスト

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -17,39 +17,20 @@ RSpec.describe Task, type: :model do
     expect(task).to be_valid
   end
 
-  describe "scopeに関するテスト" do
-    let!(:task) { Task.new(name: "aaabbb", status: "") }
-    subject { Task.get_by_name }
-    it { is_expected.to include task }
+  it "モデルに作成したscopeを使用し、タスク名による検索ができる" do
+    task = Task.new(name: "aaabbb", content: "タイトルによる検索", deadline: '2019-05-12T12:30:45', status: '未着手')
+    task.save
+    expect_task_name = Task.where(name: "aaabbb").first.name
+    search_name = Task.get_by_name(params[:name]).first.name
+    expect(search_name).to eq expect_task_name
   end
-
-
-
-  #it "モデルに作成したscopeを使用し、タスク名による検索ができる" do
-    #task = Task.new(name: "aaabbb", content: "タイトルによる検索", deadline: '2019-05-12T12:30:45', status: '未着手')
-    #task.save
-    #expect_task_title = Task.where(name: "aaabbb").first.name
-    #search_title = Task.get_by_name(@search_params).first.name
-    #expect(search_title).to eq expect_task_title
-  #end
-
-  #it "モデルに作成したscopeを使ってステータスによる検索ができる" do
-    #task = User.first.tasks.build(title: "aaabbb", summary: "タイトルけんさく", limit: '2019-04-17T12:30:45', status: '未着手')
-    #task.save
-    #expect_task_status = Task.where(status: "未着手").first.status
-    #search_status = Task.search_with_status(@search_params).first.status
-    #expect(search_status).to eq expect_task_status
-  #end
-
-  #it "モデルに作成したscopeを使ってタイトルとステータスによる検索ができる" do
-    #task = User.first.tasks.build(title: "aaabbb", summary: "タイトルけんさく", limit: '2019-04-17T12:30:45', status: '未着手')
-    #task.save
-    #expect_task_title = Task.where(title: "aaabbb").first.title
-    #search_title = Task.search_with_title(@search_params).first.title
-    #expect_task_status = Task.where(status: "未着手").first.status
-    #search_status = Task.search_with_title_and_status(@search_params).first.status
-    #expect(search_title).to eq expect_task_title
-    #expect(search_status).to eq expect_task_status
-  #end
+  
+  it "モデルに作成したscopeを使用し、statusによる検索ができる" do
+    task = Task.new(name: "aaabbb", content: "タイトルによる検索", deadline: '2019-05-12T12:30:45', status: '未着手')
+    task.save
+    expect_task_status = Task.where(status: "未着手").first.status
+    search_status = Task.get_by_status(params[:status]).first.status
+    expect(search_status).to eq expect_task_status
+  end
 
 end
