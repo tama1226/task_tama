05903597a216299908143dcf3c8344d62a2f4fd7
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-13 17:25:52 +0900

    new.htmlファイルの修正

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -26,7 +26,6 @@ RSpec.feature "タスク管理機能", type: :feature do
 
   #「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
     click_on '登録する'
-    binding.pry
   # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
   # タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
     expect(page).to have_content 'factory_name_1' 
