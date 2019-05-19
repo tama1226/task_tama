7f6a2e7a8588ad0dd236d4705e8ac2e5602ee671
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-19 19:02:20 +0900

    modify:プリコンパイル

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,8 @@ module TaskTama
     config.load_defaults 5.2
 
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-
     config.assets.initialize_on_precompile = false
+    config.active_record.default_timezone = :local
 
     config.generators do |g|
       g.test_framework :rspec,
