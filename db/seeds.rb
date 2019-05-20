2421338cb5d755ef0d425bf746e3996a52766b9f
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-20 11:21:17 +0900

    modify:Userのシードデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
       content: content,
                )
   end
+
+  30.times do |n|
+    name = Faker::Job.name
+    email = Faker::Internet.email
+    password_digest = "pass"
+    User.create(name: name,
+                 email: email,
+                 password_digest: password_digest
+                 )
+  end
+
