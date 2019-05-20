6b341d67701792ed494da79f5b300cb3b3ff363d
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-20 15:34:38 +0900

    modify:インスタンス変数をまとめる

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,8 @@ class TasksController < ApplicationController
   before_action :set_task,only:[:show,:edit,:update,:destroy]
 
   def index
-    @tasks= Task.order(created_at: :desc)
 
-    @tasks = @tasks.page(params[:page]).per(10)
+    @tasks= Task.order(created_at: :desc).page(params[:page]).per(10)
     
       if params[:sort_expired]
         @tasks=@tasks.order(deadline: :desc)
