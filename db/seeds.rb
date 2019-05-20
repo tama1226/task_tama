668a071443573abcc14ae91d452de92cc5a2593a
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-20 10:11:26 +0900

    add:taskのシードデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
 50.times do |n|
-    name = Faker::Pokemon.name
-    email = Faker::Internet.email
-    password_digest = "pass"
+    name = Faker::Name.name
+    content =  "task1"
 
-    User.create(
+    Task.create(
       name: name,
-      email: email,
-      password_digest: password
+      content: content,
                )
   end
