7d2f301d7b40eafeac7ff7ba6ddda9d79bb8b6ee
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-24 15:59:25 +0900

    add:Userモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 255 },
+             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  
+  before_validation { email.downcase! }
+  
+  validates :password_digest, presence: true, length: { minimum: 4 }
+  has_secure_password
 
   has_many :tasks
 end
