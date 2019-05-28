0db720e9a7f9aa929a635bd719e7b37214a07ab3
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-28 09:34:05 +0900

    add:管理者用のコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   root to: "tasks#index"
 
   resources :tasks
