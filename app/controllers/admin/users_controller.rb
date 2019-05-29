39b91211af6a517b4a5249d79121a2081b564f10
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-29 11:01:47 +0900

    modify:admin/users_controller.rb、各viewファイル

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,43 @@
 class Admin::UsersController < ApplicationController
   def index
     @users=User.all
+    @users= @users.page(params[:page]).per(10)
   end
+
+  def show
+  end
+
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user=User.new(user_params)
+    
+    if @user.save
+      redirect_to users_url, notice: "完了しました！"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update!(user_params)
+    redirect_to users_url, notice: "更新しました！"
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: "タスクを削除しました！"
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
 end
