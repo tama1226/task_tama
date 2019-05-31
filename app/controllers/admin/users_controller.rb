41db5cb8bc10f5e58f474c3aaedf9d14da4e04f4
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-05-31 19:36:51 +0900

    add:N+1問題を回避するため、includesメソッドを追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user,only:[:show,:edit,:update,:destroy]
 
   def index
-    @users=User.all
+    @users=User.all.includes(:tasks)
     @users= @users.page(params[:page]).per(10)
   end
 
