42214d8952f65cfb89b165d40d3868a0cc7d0d61
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-06-08 16:12:32 +0900

    modify:seedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,22 @@
 Label.create(name: 'label_6')
 Label.create(name: 'label_7')
 Label.create(name: 'label_8')
-Label.create(name: 'label_9')
-Label.create(name: 'label_10')
 
-User.create(name: 'test1'
-            email: 'test1@example.com'
-            password: 'test1'
+User.create(name: 'test1',
+            email: 'test1@example.com',
+            password: 'test1',
             password_confirmation: 'test1'
 )
 
-User.create(name: 'test2'
-            email: 'test2@example.com'
-            password: 'test2'
+User.create(name: 'test2',
+            email: 'test2@example.com',
+            password: 'test2',
             password_confirmation: 'test2'
 )
 
-User.create(name: 'test3'
-            email: 'test3@example.com'
-            password: 'test3'
+User.create(name: 'test3',
+            email: 'test3@example.com',
+            password: 'test3',
             password_confirmation: 'test3'
 )
 
