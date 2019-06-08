559c2032adb6bc9343ff08cc3b65c88ced3a33e1
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-06-08 16:00:42 +0900

    add:heroku用のseedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,32 @@
-Label.create(name: 'label_1')
-Label.create(name: 'label_2')
-Label.create(name: 'label_3')
-Label.create(name: 'label_4')
-Label.create(name: 'label_5')
+Label.create(name: 'label_6')
+Label.create(name: 'label_7')
+Label.create(name: 'label_8')
+Label.create(name: 'label_9')
+Label.create(name: 'label_10')
+
+User.create(name: 'test1'
+            email: 'test1@example.com'
+            password: 'test1'
+            password_confirmation: 'test1'
+)
+
+User.create(name: 'test2'
+            email: 'test2@example.com'
+            password: 'test2'
+            password_confirmation: 'test2'
+)
+
+User.create(name: 'test3'
+            email: 'test3@example.com'
+            password: 'test3'
+            password_confirmation: 'test3'
+)
+
+20.times do |n|
+    name = "タスク名"
+    content = "内容"
+    Task.create!(name: name,
+                 content: content
+                 )
+  end
+
